ebaa0cc4527640cf1eba49caad6516d895d6bf06
Péter Panyik <panyikpeti226@gmail.com>
2024-02-01 14:43:22 +0100

    vite routing

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,21 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        fps: resolve(__dirname, 'projects/fps/page.html'),
+        gameoflife: resolve(__dirname, 'projects/gameoflife/page.html'),
+        huehades: resolve(__dirname, 'projects/huehades/page.html'),
+        paradise: resolve(__dirname, 'projects/paradise/page.html'),
+        rangeindicator: resolve(__dirname, 'projects/rangeindicator/page.html'),
+        survival: resolve(__dirname, 'projects/survival/page.html'),
+        voxel: resolve(__dirname, 'projects/voxel/page.html'),
+      },
+    },
+  },
 })
