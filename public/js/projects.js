756b6d1a63452e0600b775177cefdd9afb7696b7
Péter Panyik <panyikpeti226@gmail.com>
2024-02-02 20:00:55 +0100

    welcome upgrade

--- public/js/projects.js
+++ public/js/projects.js
@@ -3,50 +3,57 @@
 var projectQueue = [];
 var loadingProject = false;
 
-//load a project dynamically and append to the list
-function loadProject(projectName){
-  if(!loadingProject){
-  loadingProject = true;
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      const project = JSON.parse(this.responseText);
-      var appendToList = document.getElementById("projectlisting");
-      listItem = document.createElement("LI");
-      appendToList.appendChild(listItem);
-      listItem.className = "projectlistitem";
-      projectItem = document.createElement("DIV");
-      listItem.appendChild(projectItem);
-      projectItem.className = "projectitem";
+function downloadNextProject(){
+  if(!loadingProject && projectQueue.length > 0){
+    loadingProject = true;
 
-      projectLink = document.createElement("A");
-      projectLink.setAttribute("href",project.url);
-      projectItem.appendChild(projectLink);
-      projectLink.className = "projectlink";
-
-      projectName = document.createElement("DIV");
-      projectItem.appendChild(projectName);
-      projectName.className = "projectname";
-      projectName.innerHTML = project.title;
-      projectImage = document.createElement("IMG");
-      projectItem.appendChild(projectImage);
-      projectImage.className = "projpic";
-      projectImage.setAttribute("src",project.thumb);
-      projectDesc = document.createElement("DIV");
-      projectItem.appendChild(projectDesc);
-      projectDesc.className = "projectdesc";
-      projectDesc.innerHTML = project.desc;
-    }
-    loadingProject = false;
-    if(projectQueue.length > 0){
-      var nextProject = projectQueue[0];
-      projectQueue.shift();
-      loadProject(nextProject);
-    }
-  };
-  xmlhttp.open("GET", "projects/"+projectName+"/config.json", true);
-  xmlhttp.send();
-  }else{
-    projectQueue.push(projectName);
+    var nextProject = projectQueue[0];
+    projectQueue.shift();
+    var listItem = nextProject.anchor;
+    projectName = nextProject.name;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        const project = JSON.parse(this.responseText);
+        
+        listItem.className = "projectlistitem";
+        projectItem = document.createElement("DIV");
+        listItem.appendChild(projectItem);
+        projectItem.className = "projectitem";
+  
+        projectLink = document.createElement("A");
+        projectLink.setAttribute("href",project.url);
+        projectItem.appendChild(projectLink);
+        projectLink.className = "projectlink";
+  
+        projectName = document.createElement("DIV");
+        projectItem.appendChild(projectName);
+        projectName.className = "projectname";
+        projectName.innerHTML = project.title;
+        projectImage = document.createElement("IMG");
+        projectItem.appendChild(projectImage);
+        projectImage.className = "projpic";
+        projectImage.setAttribute("src",project.thumb);
+        projectDesc = document.createElement("DIV");
+        projectItem.appendChild(projectDesc);
+        projectDesc.className = "projectdesc";
+        projectDesc.innerHTML = project.desc;
+      }
+      loadingProject = false;
+      
+      downloadNextProject();
+    };
+    xmlhttp.open("GET", "projects/"+projectName+"/config.json", true);
+    xmlhttp.send();
   }
+}
+
+
+//load a project dynamically and append to the list
+function loadProject(projectName, id){
+  var appendToList = document.getElementById(id);
+  listItem = document.createElement("LI");
+  appendToList.appendChild(listItem);
+  projectQueue.push({name: projectName, anchor: listItem});
+  downloadNextProject();
 }
--- public/js/welcome.js
+++ public/js/welcome.js
@@ -1,4 +1,5 @@
 var writinw = false;
+var lastChild;
 
 function sleep(ms) {
   return new Promise(function (resolve){
@@ -12,49 +13,67 @@ animateWelcome();
 async function animateWelcome(){
 
 var witer = 0;
-var updater = setInterval(update, 500);
+var updater = setInterval(update, 200);
 
-var s = document.getElementById("welcometxt").innerHTML;
-document.getElementById("welcometxt").innerHTML = '_';
-document.getElementById("welcometxt").style.visibility = "visible";
 
-await sleep(1000);
-for(var wchar = 1;wchar <= s.length;wchar++){
-  if(s.charAt(wchar - 2) == '!'){
-    writinw = false;
-    await sleep(1000);
-  }
-  if(s.charAt(wchar) + "" + s.charAt(wchar+1) == "😄"){
-    writinw = false;
-    await sleep(1250);
-  }
-  if(s.charAt(wchar-1) + "" + s.charAt(wchar) == "😄"){
-    wchar++;
-  }
-  writinw = true;
-  if(s.charAt(wchar - 1) != '<'){
-  document.getElementById("welcometxt").innerHTML = s.substring(0,wchar) + '_';
-  }
-  await sleep(Math.random() * 60 + 50);
+var welcomeText = document.getElementById("welcometxt");
+var childContents = [];
+var welcomeChildren = welcomeText.children;
+for(var c = 0; c < welcomeChildren.length;c++){
+  var child = welcomeChildren[c];
+  childContents.push({div: child, text: child.innerHTML});
+  child.innerHTML = "";
 }
-writinw = false;
 
 
+welcomeText.style.visibility = "visible";
 
+for(var c = 0; c < welcomeChildren.length;c++){
+  var childContent = childContents[c];
+  var child = childContent.div;
+  lastChild = child;
+  var s = childContent.text;
+
+  await sleep(100);
+  for(var wchar = 1;wchar <= s.length;wchar++){
+    if(s.charAt(wchar - 2) == '!'){
+      writinw = false;
+      await sleep(500);
+    }
+    if(s.charAt(wchar) + "" + s.charAt(wchar+1) == "😄"){
+      writinw = false;
+      await sleep(150);
+    }
+    if(s.charAt(wchar-1) + "" + s.charAt(wchar) == "😄"){
+      wchar++;
+    }
+    writinw = true;
+    if(s.charAt(wchar - 1) != '<'){
+      child.innerHTML = s.substring(0,wchar) + '_';
+    console.log(s.substring(0,wchar) + '_');
+    }
+    await sleep(Math.random() * 30 + 20);
+  }
+  writinw = false;
+
+  lastChild = null;
+  child.innerHTML = childContent.text;
+}
+clearInterval(updater);
 
 function update() {
-if(!writinw){
-var currentws = document.getElementById("welcometxt").innerHTML;
-let sslen = currentws.length - 1;
-if(witer % 2 == 1){
-if(currentws.charAt(sslen) != '_'){
-document.getElementById("welcometxt").innerHTML = currentws.substring(0,sslen - 1) + "_";
-}
-}else{
-document.getElementById("welcometxt").innerHTML = currentws.substring(0,sslen) + ' \u200b';
-}
-witer++;
-}
+  if(!writinw && lastChild != undefined && lastChild != null){
+    var currentws = lastChild.innerHTML;
+    let sslen = currentws.length - 1;
+    if(witer % 2 == 1){
+      if(currentws.charAt(sslen) != '_'){
+        lastChild.innerHTML = currentws.substring(0,sslen - 1) + "_";
+      }
+    }else{
+      lastChild.innerHTML = currentws.substring(0,sslen) + ' \u200b';
+    }
+    witer++;
+  }
 }
 
 
