979676b38cb618d89e4b9b2e28b07e749c855d21
Péter Panyik <panyikpeti226@gmail.com>
2024-02-10 11:50:33 +0100

    projects update

---
+++ src/projects/main.ts
@@ -0,0 +1,6 @@
+import navigationHtml from "./navigation.html.ts";
+
+var div = document.createElement("div");
+
+div.innerHTML = navigationHtml;
+document.body.appendChild(div);
---
+++ src/projects/navigation.html.ts
@@ -0,0 +1,5 @@
+export default /* html */ ` 
+<a class="homebutton button" href="../../index.html">
+    <img src="../../img/home_icon.svg" alt="" class="homebuttonicon">
+</a>
+`
